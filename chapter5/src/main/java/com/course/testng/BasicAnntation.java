29d8bf1a93501f9b4dcd9e0de8a9615b6aa56376
YQR <18435146718@163.com>
2018-06-04 16:15:54 +0800

    提交

---
+++ chapter5/src/main/java/com/course/testng/BasicAnntation.java
@@ -0,0 +1,15 @@
+package com.course.testng;
+
+import org.testng.annotations.Test;
+
+public class BasicAnntation {
+    @Test
+    public void tesCase1(){
+        System.out.println("这是测试用例1");
+    }
+   @Test
+    public void testCase2(){
+       System.out.println("这是测试用例2");
+   }
+
+}
